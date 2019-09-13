2601fafc34c7fff18d8d4a6ae777ef5b7af31383
Denis Ojdanic <denis.ojdanic@yahoo.com>
2019-09-13 09:06:39 +0200

    remove wrapper

--- mdcstream-wrapper/utilities/print.m
+++
@@ -1,8 +0,0 @@
-
-function print(value, string)
-
-fprintf(string + " = ");
-fprintf(' %.2f ', value(1 : end));
-fprintf('\n');
-
-end
--- mdcstream-wrapper/utilities/randf.m
+++
@@ -1,7 +0,0 @@
-
-
-function value = randf(lower, upper)
-
-value = lower + (upper - lower) * rand();
-end
-
